fce4641e704c4da0b9b2ce93932eda7ca77ed261
ndimhypervol <jamie.m.kass@gmail.com>
2019-01-21 07:58:26 -0500

    real model stats

---
+++ R/evalStats.R
@@ -0,0 +1,28 @@
+#' @export
+
+evalStats <- function(occs.train, bg.train, occs.test, mod, abs.auc.diff) {
+  # calculate auc on training and testing data
+  auc.train <- dismo::evaluate(occs.train, bg.train, mod)@auc
+  auc.test <- dismo::evaluate(occs.test, bg.train, mod)@auc
+  # calculate auc diff
+  auc.diff <- auc.train - auc.test
+  if(abs.auc.diff == TRUE) auc.diff <- abs(auc.diff)
+  # get model predictions for training and testing data
+  pred.train <- predict(mod, occs.train)
+  pred.test <- predict(mod, occs.test)
+  # get 10 percentile predicted value
+  occs.train.n <- nrow(occs.train)
+  if(occs.train.n < 10) {
+    pct10.train <- ceiling(occs.train.n * 0.1)
+  } else {
+    pct10.train <- floor(occs.train.n * 0.1)
+  }
+  pct10.train.thr <- sort(pred.train)[pct10.train]
+  or10.test <- mean(pred.test < pct10.train.thr)
+  min.train.thr <- min(pred.train)
+  orMin.test <- mean(pred.test < min.train.thr)
+
+  stats <- c(auc.test, auc.diff, orMin.test, or10.test)
+
+  return(stats)
+}
--- R/nullSDMs.R
+++ R/nullSDMs.R
@@ -5,32 +5,42 @@
 # occs <- as.data.frame(bv$gbif$data$Bradypus_variegatus[,2:3])
 # occs <- occs[!duplicated(occs),]
 # envs <- raster::stack(list.files(path=paste(system.file(package='dismo'), '/ex', sep=''), pattern='grd', full.names=TRUE))
-envs <- raster::mask(envs, envs$biome)
+# envs <- raster::mask(envs, envs$biome)
 # which(rowSums(is.na(raster::extract(envs, occs))) > 0)
 # bg <- dismo::randomPoints(envs[[1]], 10000)
 # mod.args <- list(fc = "LQ", rm = 1)
-# folds <- ENMeval::get.block(occs, bg)
-# occs.folds <- folds$occ.grp
-# bg.folds <- folds$bg.grp
+# partitions.kfold <- ENMeval::get.block(occs, bg)
+# partitions.kfold$occs <- partitions.kfold$occ.grp
+# partitions.kfold$bg <- partitions.kfold$bg.grp
 # envs.xy <- raster::rasterToPoints(envs[[1]], spatial = TRUE)
-# envs.folds <- ENMeval::get.block(occs, envs.xy@coords)$bg.grp
-# x1 <- nullSDMs(occs, envs, bg, occs.folds, bg.folds, envs.folds, dismo::maxent, mod.args, 10, "biome")
-# x2 <- nullSDMs(occs, envs, bg, occs.folds, bg.folds, envs.folds, maxnet::maxnet, mod.args, 10, "biome")
+# partitions.kfold$envs <- ENMeval::get.block(occs, envs.xy@coords)$bg.grp
+# partitions.split <- ENMeval::get.randomkfold(occs, bg, 2)
+# x1 <- nullSDMs(occs, envs, bg, partitions, dismo::maxent, mod.args, 10, "biome")
+# x2 <- nullSDMs(occs, envs, bg, partitions.kfold, maxnet::maxnet, mod.args, 5, "kfold", "biome")
 
-nullSDMs <- function(occs, envs, bg, occs.folds, bg.folds, envs.folds,
-                     mod.fun, mod.args, no.iter, categoricals=NULL, abs.auc.diff = FALSE) {
+nullSDMs <- function(occs, envs, bg, partitions,
+                     mod.fun, mod.args, no.iter, eval = c("split", "kfold"),
+                     categoricals = NULL, abs.auc.diff = FALSE) {
 
-  # define new raster (envs.cv) with values equal to folds based on envs.folds
-  envs.xy <- rasterToPoints(envs[[1]], spatial = TRUE)
-  envs.cellNo <- raster::extract(envs[[1]], envs.xy, cellnumbers = TRUE)
-  envs.cv <- envs[[1]]
-  envs.cv[envs.cellNo[,1]] <- envs.folds
+  if(eval == "split") {
+    # partition number equals 1
+    nk <- 1
 
-  # get number of occurrence points by fold
-  occs.folds.tbl <- table(occs.folds)
 
-  # get total number of folds (k)
-  nk <- length(unique(occs.folds))
+  }
+
+  if(eval == "kfold") {
+    # get total number of partitions (k)
+    nk <- length(unique(partitions$occs))
+    # define new raster (envs.cv) with values equal to partitions based on partitions$envs
+    envs.xy <- rasterToPoints(envs[[1]], spatial = TRUE)
+    envs.cellNo <- raster::extract(envs[[1]], envs.xy, cellnumbers = TRUE)
+    envs.cv <- envs[[1]]
+    envs.cv[envs.cellNo[,1]] <- partitions$envs
+  }
+
+  # get number of occurrence points by partition
+  occs.parts.tbl <- table(partitions$occs)
 
   # get environmental values for occs and bg
   occs.vals <- as.data.frame(raster::extract(envs, occs))
@@ -39,34 +49,73 @@ nullSDMs <- function(occs, envs, bg, occs.folds, bg.folds, envs.folds,
   # get model function name
   mod.fun.name <- as.character(substitute(mod.fun))[3]
 
-  # initialize data frames to collect evaluation stats for each fold
+  # initialize data frames to collect evaluation stats for each partition
   # per iteration and their averages
-  fields.avg <- c("iter", "full.auc.train", "avg.auc.train", "std.auc.train",
-                  "avg.auc.test", "std.auc.test", "avg.auc.diff", "std.auc.diff",
-              "avg.or.min", "std.or.min", "avg.or.10", "std.or.10")
-  null.stats.avg <- data.frame(matrix(nrow = no.iter, ncol = length(fields.avg)))
-  names(null.stats.avg) = fields.avg
+  tbl.fields <- c("auc.train", "avg.auc.test", "std.auc.test", "avg.auc.diff",
+                  "std.auc.diff", "avg.or.min", "std.or.min", "avg.or.10", "std.or.10")
+  k.fields <- c("auc.test", "auc.diff", "or.min", "or.10")
+  real.stats <- data.frame(matrix(nrow = 1, ncol = length(tbl.fields)))
+  names(real.stats) = tbl.fields
+  null.stats <- data.frame(matrix(nrow = no.iter, ncol = length(tbl.fields)))
+  names(null.stats) = tbl.fields
+
+
+
+  ############################## #
+  # build real model ####
+  ############################## #
+
+  mod.args.real <- buildModArgs(mod.fun.name, mod.args, occs.vals, bg.vals)
+  mod.real <- do.call(mod.fun, mod.args.real)
+  # calculate training auc
+  real.stats$auc.train <- dismo::evaluate(occs.vals, bg.vals, mod.real)@auc
+  kstats <- data.frame(matrix(nrow = nk, ncol = length(k.fields)))
+  names(kstats) <- k.fields
+
+  for(k in 1:nk) {
+    occs.train.real.k <- occs.vals[partitions$occs != k, ]
+    occs.test.real.k <- occs.vals[partitions$occs == k, ]
+    bg.train.k <- bg.vals[partitions$bg != k, ]
+
+    mod.args.real.k <- buildModArgs(mod.fun.name, mod.args, occs.train.real.k, bg.train.k)
+    mod.real.k <- do.call(mod.fun, mod.args.real.k)
+    kstats[k,] <- evalStats(occs.train.real.k, bg.train.k, occs.test.real.k,
+                               mod.real.k, abs.auc.diff)
+    message(sprintf("Completed real model and evaluation for partition %i.", k))
+  }
+  # fill in rest of real model statistics
+  real.stats$avg.auc.test <- mean(kstats$auc.test)
+  real.stats$std.auc.test <- sd(kstats$auc.test)
+  real.stats$avg.auc.diff <- mean(kstats$auc.diff)
+  real.stats$std.auc.diff <- sd(kstats$auc.diff)
+  real.stats$avg.or.min <- mean(kstats$or.min)
+  real.stats$std.or.min <- sd(kstats$or.min)
+  real.stats$avg.or.10 <- mean(kstats$or.10)
+  real.stats$std.or.10 <- sd(kstats$or.10)
+
+
+
+  ############################## #
+  # build null models ####
+  ############################## #
+
+  # initialize list to record stats for null iteration i
   null.stats.i <- list()
 
-
-
-  # iteratively build null models
   for(i in 1:no.iter) {
-    # initialize data frame for fold statistics for current iteration
-    fields.i <- c("auc.train", "auc.test", "auc.diff", "or.min", "or.10")
-    null.stats.i[[i]] <- data.frame(matrix(nrow = nk, ncol = length(fields.i)))
-    names(null.stats.i[[i]]) <- fields.i
+    # initialize data frame for partition statistics for current iteration
+    null.stats.i[[i]] <- data.frame(matrix(nrow = nk, ncol = length(k.fields)))
+    names(null.stats.i[[i]]) <- k.fields
 
     # sample random occurrences
     occs.null <- list()
     for(k in 1:nk) {
       envs.k <- envs.cv
       envs.k[envs.k != k] <- NA
-      occs.null.xy <- dismo::randomPoints(envs.k, occs.folds.tbl[k])
+      occs.null.xy <- dismo::randomPoints(envs.k, occs.parts.tbl[k])
       occs.null[[k]] <- as.data.frame(extract(envs, occs.null.xy))
     }
     occs.null.all <- do.call(rbind, occs.null)
-
 
     mod.args.i <- buildModArgs(mod.fun.name, mod.args, occs.null.all, bg.vals)
     mod.i <- do.call(mod.fun, mod.args.i)
@@ -74,10 +123,10 @@ nullSDMs <- function(occs, envs, bg, occs.folds, bg.folds, envs.folds,
 
     for(k in 1:nk) {
       # get training and testing data for current iteration
-      # (non-k training and k testing folds)
+      # (non-k training and k testing partitions)
       occs.null.train <- do.call(rbind, occs.null[-k])
-      bg.train <- bg.vals[bg.folds != k, ]
-      occs.test <- occs.vals[occs.folds == k, ]
+      bg.train <- bg.vals[partitions$bg != k, ]
+      occs.test <- occs.vals[partitions$occs == k, ]
 
       # convert fields for categorical data to factor class
       if(!is.null(categoricals)) {
@@ -92,41 +141,20 @@ nullSDMs <- function(occs, envs, bg, occs.folds, bg.folds, envs.folds,
       # build the model
       mod.k <- do.call(mod.fun, mod.args.k)
 
-      # calculate auc on training and testing data
-      auc.train.k <- dismo::evaluate(occs.null.train, bg.train, mod.k)@auc
-      auc.test.k <- dismo::evaluate(occs.test, bg.train, mod.k)@auc
-      # calculate auc diff
-      auc.diff.k <- auc.train.k - auc.test.k
-      if(abs.auc.diff == TRUE) auc.diff.k <- abs(auc.diff.k)
-      # get model predictions for training and testing data
-      pred.train.k <- predict(mod.k, occs.null.train)
-      pred.test.k <- predict(mod.k, occs.test)
-      # get 10 percentile predicted value
-      occs.null.train.n <- nrow(occs.null.train)
-      if(occs.null.train.n < 10) {
-        pct10.train.k <- ceiling(occs.null.train.n * 0.1)
-      } else {
-        pct10.train.k <- floor(occs.null.train.n * 0.1)
-      }
-      pct10.train.k.thr <- sort(pred.train.k)[pct10.train.k]
-      or10.test.k <- mean(pred.test.k < pct10.train.k.thr)
-      min.train.k.thr <- min(pred.train.k)
-      orMin.test.k <- mean(pred.test.k < min.train.k.thr)
+      null.stats.i[[i]][k,] <- evalStats(occs.null.train, bg.train, occs.test,
+                                         mod.k, abs.auc.diff)
 
-      null.stats.i[[i]][k,] <- c(auc.train.k, auc.test.k, auc.diff.k,
-                                 orMin.test.k, or10.test.k)
-
-      message(sprintf("Completed model and evaluation for fold %i of iteration %i.", k, i))
+      message(sprintf("Completed null model and evaluation for partition %i of iteration %i.", k, i))
     }
-    # average fold statistics
+    # average partition statistics
     means <- apply(null.stats.i[[i]], 2, mean)
     stds <- apply(null.stats.i[[i]], 2, sd)
     # alternate the above values for data frame
     stats <- c(rbind(means, stds))
-    null.stats.avg[i,] <- c(i, auc.train.i, stats)
+    null.stats[i,] <- c(auc.train.i, stats)
   }
 
-  out <- list(null.stats.avg = null.stats.avg, null.stats.i = null.stats.i)
+  out <- list(real.stats = real.stats, null.stats = null.stats, null.stats.i = null.stats.i)
   return(out)
 
 }
